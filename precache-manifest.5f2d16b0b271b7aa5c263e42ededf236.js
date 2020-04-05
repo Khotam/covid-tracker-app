15dc2634641cdf07a5e1536f67629bd2d609aed3
Khotam <bakhromov17@gmail.com>
2020-04-05 23:24:48 +0500

    Updates

---
+++ precache-manifest.5f2d16b0b271b7aa5c263e42ededf236.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6698fb15b4771d739a6ddb8f0b5d8793",
+    "url": "/covid-tracker-app/index.html"
+  },
+  {
+    "revision": "baf6d3d3a9364dc2b37d",
+    "url": "/covid-tracker-app/static/css/main.a940e156.chunk.css"
+  },
+  {
+    "revision": "61405eb340565e0d9910",
+    "url": "/covid-tracker-app/static/js/2.0ec73d82.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracker-app/static/js/2.0ec73d82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baf6d3d3a9364dc2b37d",
+    "url": "/covid-tracker-app/static/js/main.90fd8ff5.chunk.js"
+  },
+  {
+    "revision": "1ba81b65d68f67e04cc9",
+    "url": "/covid-tracker-app/static/js/runtime-main.9b58a90a.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-tracker-app/static/media/covid.d7265326.png"
+  }
+]);
